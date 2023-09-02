c311a6a1eeaea10e66d9795ae169d9fdb06aa701
asmaatajeldein <asmaatajeldein@gmail.com>
2023-09-02 14:22:40 +0300

    build: add routermodule to add nested routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,26 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DeckModule } from './deck/deck.module';
+import { FlashcardModule } from './flashcard/flashcard.module';
+import { RouterModule, Routes } from '@nestjs/core';
 
+const routes: Routes = [
+  {
+    path: '/decks',
+    module: DeckModule,
+    children: [{ path: ':deckId/flashcards', module: FlashcardModule }],
+  },
+];
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, UserModule, DeckModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RouterModule.register(routes),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    DeckModule,
+    FlashcardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
